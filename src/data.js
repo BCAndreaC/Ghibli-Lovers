e94bcf258b4e3ffa230c3e5db797fa8c33723091
BCAndreaC <126895867+BCAndreaC@users.noreply.github.com>
2023-04-10 18:25:06 -0500

    Html modificacion, filter y sort de Characters.
    Co-authored-by: Marissa Vargas-Sánchez <thatmare@users.noreply.github.com>

--- src/data.js
+++ src/data.js
@@ -9,8 +9,22 @@ export const sortByOldest = (films) => {
   return sortedData;
 };
 
-// Función de filtrado por director
+export const sortByAZ = (films) => {
+  const sortedData = films.sort((a,z) => (a.name > z.name ? 1 : -1));
+  return sortedData;
+};
+
+export const sortByZA = (films) => {
+  const sortedData = films.sort ((a,z) => (a.name < z.name ? 1 : -1));
+  return sortedData;
+};
+
+//Función de filtrado por director
 export const filterDirector = (films, selectDirectors) =>{
   return films.filter((film) => film.director.includes(selectDirectors));
 };
 
+//Función de personajes filtrados por género
+export const filterGender = (characters, selectGender) => {
+  return characters.filter((character) => character.gender.includes(selectGender));
+};
--- src/main.js
+++ src/main.js
@@ -1,92 +1,76 @@
-import { filterDirector, sortByNewest, sortByOldest } from './data.js';
+import { sortByNewest, sortByOldest, filterDirector, filterGender, sortByAZ, sortByZA } from './data.js';
 //Nos dimos cuenta que data puede ser cualquier palabra ya que funciona "como una variable"
 import data from './data/ghibli/ghibli.js';
 //Declaramos una variable que recupera los films de la data
 const dataGhibli = data.films
-
-const characterContainer = document.getElementById('characterContainer')
 const people = dataGhibli.flatMap(movie => movie.people);
-
-function displayCharacters(filmCharacters){
-  filmCharacters.forEach((people) => {
-    const createFigure = document.createElement("figure");
-    const createImg = document.createElement("img");
-    const createName = document.createElement("p");
-    const createAge = document.createElement("p");
-    const createGender = document.createElement("p");
-
-    createFigure.setAttribute("class", "filmContainer");
-    createImg.setAttribute("src", people.img);
-    createImg.setAttribute("class", "peopleImg");
-    createName.innerHTML = people.name;
-    createName.setAttribute("class", "characterName");
-    createAge.innerHTML = people.age;
-    createAge.setAttribute("class", "characterAge");
-    createGender.innerHTML = people.gender;
-    createGender.setAttribute("class", "characterGender");
-    
-    createFigure.appendChild(createImg);
-    createFigure.appendChild(createName);
-    createFigure.appendChild(createAge);
-    createFigure.appendChild(createGender);
-    characterContainer.appendChild(createFigure);
-  })
-}
-
-//Función de menú hamburger
+const btnHome = document.getElementById ('btn-home');
+const btnMovies = document.getElementById('btn-movies');
+const btnCharacters = document.getElementById('btn-characters');
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const sortOptions = document.getElementById('sortOptions');
+const sortCharacters = document.getElementById('sortCharacters');
+const filterOptions = document.getElementById('directorByFilter');
+const filterByGender = document.getElementById('genderFilter');
+const movieContainer = document.getElementById("movieContainer");
+const characterContainer = document.getElementById('characterContainer');
 
-hamburger.addEventListener("click", mobileMenu);
-
+//Función de menú hamburger
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
+hamburger.addEventListener("click", mobileMenu);
 
-const movieContainer = document.getElementById("movieContainer");
-const sortOptions = document.getElementById('sortOptions');
-const filterOptions = document.getElementById('directorByFilter');
-
-// Función de despliegue de películas
-function displayFilms(films) {
-  films.forEach((film) => {
-    const createFigure = document.createElement("figure");
-    const createImg = document.createElement("img");
-    const createH3 = document.createElement("h3");
-    const createP = document.createElement("p");
-
-    createFigure.setAttribute("class", "filmContainer");
-    createImg.setAttribute("src", film.poster);
-    createImg.setAttribute("class", "filmPoster");
-    createH3.innerHTML = film.title;
-    createH3.setAttribute("class", "filmTitle");
-    createP.innerHTML = film.release_date;
-    createP.setAttribute("class", "filmYear");
-
-    createFigure.appendChild(createImg);
-    createFigure.appendChild(createH3);
-    createFigure.appendChild(createP);
-    movieContainer.appendChild(createFigure);
-  })
+//Función para ingresar a la página Home
+function displayHomepage(){
+  //Esconder Movie
+  const movieWrap = document.getElementById('movieWrap');
+  movieWrap.style.display = "none";
+  //Esconder Personajes
+  const characterWrap = document.getElementById('characterWrap');
+  characterWrap.style.display = "none";
+  //Mostrar home
+  const homeWrap = document.getElementById('homeWrap');
+  homeWrap.style.display = "flex";
 }
+btnHome.addEventListener('click', displayHomepage);
 
-// const btnCharacters = document.getElementsByClassName("btn-characters");
-// btnCharacters.addEventListener("click", displayCharacters(people));
+function displayMoviePage(){
+  //Ocultar el carrusel
+  const homeWrap = document.getElementById('homeWrap');
+  homeWrap.style.display = "none";
+  //Ocultar los personajes
+  const characterWrap = document.getElementById('characterWrap');
+  characterWrap.style.display = "none";
+  //Mostrar barra de sort, filter
+  const movieWrap = document.getElementById('movieWrap');
+  movieWrap.style.display = "flex";  
+}
+btnMovies.addEventListener('click', displayMoviePage);
 
-// const btnMovies = document.getElementsByClassName("btn-movies");
-// btnMovies.addEventListener("click", displayFilms(dataGhibli));
+function displayCharacterPage(){
+  //Esconder home
+  const homeWrap = document.getElementById('homeWrap');
+  homeWrap.style.display = "none";
+  //Esconder Movie
+  const movieWrap = document.getElementById('movieWrap');
+  movieWrap.style.display = "none";
+  //Mostrar Personajes
+  const characterWrap = document.getElementById('characterWrap');
+  characterWrap.style.display = "flex";
+}
+btnCharacters.addEventListener('click', displayCharacterPage);
 
-// displayFilms(dataGhibli);
-// displayCharacters(people);
-
+//Ordenar películas por más recientes
 function sortByNewestMovies() {
   const newestOrder = sortByNewest(dataGhibli)
   movieContainer.innerHTML = '';
   return displayFilms(newestOrder);
 }
 
-
+//Ordenar películas por más antiguas
 function sortByOldestMovies() {
   const oldestOrder = sortByOldest(dataGhibli)
   movieContainer.innerHTML = '';
@@ -102,6 +86,26 @@ sortOptions.addEventListener('click', function(){
   }
 });
 
+function sortByAZCharacters(){
+  const azOrder = sortByAZ(people)
+  characterContainer.innerHTML = '';
+  return displayCharacters(azOrder);
+}
+
+function sortByZACharacters(){
+  const zaOrder = sortByZA(people)
+  characterContainer.innerHTML = '';
+  return displayCharacters(zaOrder);
+}
+
+sortCharacters.addEventListener('click', function(){
+  if(sortCharacters.value === 'A-Z'){
+    sortByAZCharacters();
+  } else if (sortCharacters.value === 'Z-A'){
+    sortByZACharacters();
+  }
+})
+
 // Escuchador de evento para la función de filtrado por director
 filterOptions.addEventListener("change", () => {
   const directorSelected = filterOptions.value;
@@ -109,3 +113,63 @@ filterOptions.addEventListener("change", () => {
   movieContainer.innerHTML = '';
   displayFilms(directorFiltered);
 });
+
+// Escuchador de evento para la función de filtrar por género
+filterByGender.addEventListener("change", () => {
+  const genderSelected = filterByGender.value;
+  const genderFiltered = filterGender(people, genderSelected);
+  characterContainer.innerHTML = '';
+  displayCharacters(genderFiltered);
+})
+
+// Función para despliegue de personajes
+function displayCharacters(filmCharacters){
+  filmCharacters.forEach((people) => {
+    const createFigure = document.createElement("figure");
+    const createImg = document.createElement("img");
+    const createName = document.createElement("p");
+    const createAge = document.createElement("p");
+    const createGender = document.createElement("p");
+
+    createFigure.setAttribute("class", "filmContainer");
+    createImg.setAttribute("src", people.img);
+    createImg.setAttribute("class", "filmPoster");
+    createName.innerHTML = people.name;
+    createName.setAttribute("class", "filmTitle");
+    createAge.innerHTML = people.age;
+    createAge.setAttribute("class", "filmInformation");
+    createGender.innerHTML = people.gender;
+    createGender.setAttribute("class", "filmInformation");
+    
+    createFigure.appendChild(createImg);
+    createFigure.appendChild(createName);
+    createFigure.appendChild(createAge);
+    createFigure.appendChild(createGender);
+    characterContainer.appendChild(createFigure);
+  })
+}
+displayCharacters(people);
+
+// Función para despliegue de películas
+function displayFilms(films) {
+  films.forEach((film) => {
+    const createFigure = document.createElement("figure");
+    const createImg = document.createElement("img");
+    const createH3 = document.createElement("h3");
+    const createP = document.createElement("p");
+
+    createFigure.setAttribute("class", "filmContainer");
+    createImg.setAttribute("src", film.poster);
+    createImg.setAttribute("class", "filmPoster");
+    createH3.innerHTML = film.title;
+    createH3.setAttribute("class", "filmTitle");
+    createP.innerHTML = film.release_date;
+    createP.setAttribute("class", "filmInformation");
+
+    createFigure.appendChild(createImg);
+    createFigure.appendChild(createH3);
+    createFigure.appendChild(createP);
+    movieContainer.appendChild(createFigure);
+  })
+}
+displayFilms(dataGhibli);
